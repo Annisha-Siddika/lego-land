e8ad3de3eb2755eb7ed7228d26e0dd62d03f160d
AnnishaSiddika <annisha2112@gmail.comm>
2023-05-21 20:49:40 +0600

    sort and search system added

--- src/pages/AllToys/AllToys.jsx
+++ src/pages/AllToys/AllToys.jsx
@@ -1,20 +1,57 @@
 import { useEffect, useState } from "react";
 
 const AllToys = () => {
-    const [toys, setToys] = useState([]);
+  const [toys, setToys] = useState([]);
+  const [sortOrder, setSortOrder] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
+  useEffect(() => {
+    fetchToys();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [sortOrder]); 
 
-    useEffect(()=>{
-        fetch('https://assignment-11-server-five-sable.vercel.app/alltoys')
-        .then(res => res.json())
-        .then(data => {
-            setToys(data)
-            console.log(data)
-        })
-    },[])
+  const fetchToys = async () => {
+    try {
+      const response = await fetch(`https://assignment-11-server-five-sable.vercel.app/alltoys?sort=${sortOrder}`);
+      const data = await response.json();
+      setToys(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    return (
-        <div className="pt-40">
-            <h2 className="text-xl md:text-2xl font-bold italic text-center py-8">All Toys</h2>
+  const handleSortChange = (e) => {
+    setSortOrder(e.target.value);
+  };
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const filteredToys = toys.filter((toy) =>
+    toy.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div className="pt-40">
+      <h2 className="text-xl md:text-2xl font-bold italic text-center py-8">All Toys</h2>
+      <div className="flex justify-evenly items-center">
+      <div className="mb-4">
+        <label className="mr-2">Sort Order:</label>
+        <select value={sortOrder} onChange={handleSortChange} className="border-2 outline-none border-pink-500 rounded-md p-2">
+          <option value="asc">Ascending</option>
+          <option value="desc">Descending</option>
+        </select>
+      </div>
+      <div className="mb-4">
+        <label className="mr-2">Search:</label>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={handleSearch}
+          className="border-2 outline-none border-pink-500 rounded-md p-2"
+          placeholder="Search by toy name"
+        />
+      </div>
+      </div>
             <table className="w-11/12 mx-auto mb-20 border-b-8 border-pink-500">
                 <thead className="bg-pink-500 text-white">
                     <tr>
@@ -28,7 +65,7 @@ const AllToys = () => {
                     </tr>
                 </thead>
                 <tbody className="text-center font-semibold">
-                    {toys.map((toy, index) => (
+                    {filteredToys.map((toy, index) => (
                         <tr key={index} className={index % 2 === 0 ? 'bg-gray-100' : ''}>
                             <td className="px-4 py-2 w-28">
                                 <img src={toy.pictureUrl} alt="" /></td>
